18cac0324e126cebf3f64c1274627616e3a66d74
Hindu Gourishetti <hindugourishetti3@gmail.com>
2026-08-13 13:12:33 +0530

    Set up SQLite database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from .config import Config
+from .database import db
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     @app.route("/")
     def home():
--- app/config.py
+++ app/config.py
@@ -3,5 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///nexus.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
