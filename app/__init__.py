0ddfd7385007ad970e244dec619b99bfdae612a6
Hindu Gourishetti <hindugourishetti3@gmail.com>
2026-08-13 12:56:03 +0530

    Add application configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from .config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     @app.route("/")
     def home():
-        return "<h1>Welcome to Nexus 🚀</h1>"
+        return "<h1>Nexus is Alive 🚀</h1>"
 
     return app
